6a15d8e9883e107231dd376901f08af52f0d2bef
Avishek Datta <code.avishek.datta@gmail.com>
2020-05-06 00:11:58 +0200

    Commit to complete operations on Linked List.

--- src/linkedlist/LinkedList.java
+++ src/linkedlist/LinkedList.java
@@ -1,6 +1,7 @@
 package linkedlist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class LinkedList<T extends Comparable<T>> implements Cloneable {
@@ -35,8 +36,10 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 */
 	public void printNodes() {
 		Node<T> curr = head;
-		while (null != curr.getNext())
-			System.out.println(curr.toString() + " -> ");
+		while (null != curr) {
+			System.out.print(curr.toString() + " ");
+			curr = curr.getNext();
+		}
 	}
 
 	/**
@@ -45,21 +48,28 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	public int countNodes() {
 		Node<T> curr = head;
 		int count = 0;
-		
+		while (null != curr) {
+			count = count + 1;
+			curr = curr.getNext();
+		}
+		return count;
+
 	}
 
 	/**
 	 * Return the first element in the linked list.
 	 */
 	public T popElement() {
-
+		Node<T> curr = head;
+		head = curr.getNext();
+		return curr.getData();
 	}
 
 	/**
 	 * Delete all the elements in the linked list.
 	 */
 	public void deleteLinkedList() {
-
+		head = null;
 	}
 
 	/**
@@ -67,7 +77,24 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * than n.
 	 */
 	public void insertNth(int n, T data) {
+		Node<T> curr = head;
+		Node<T> next = null;
+		Node<T> node = new Node<T>(data);
 
+		if (n == 0) {
+			node.setNext(curr);
+			head = node;
+		} else {
+			while (n-- > 1) {
+				curr = curr.getNext();
+				if (null == curr) {
+					return;
+				}
+				next = curr.getNext();
+			}
+			node.setNext(next);
+			curr.setNext(node);
+		}
 	}
 
 	/**
@@ -75,14 +102,34 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * list is sorted in ascending order.
 	 */
 	public void insertSorted(T data) {
+		Node<T> curr = head;
+		Node<T> node = new Node<T>(data);
+		Node<T> prev = head;
 
+		if (head.getData().compareTo(data) > 0) {
+			node.setNext(curr);
+			head = node;
+		} else {
+			while (null != curr.getNext() && curr.getNext().getData().compareTo(data) < 0) {
+				prev = curr;
+				curr = curr.getNext();
+			}
+			prev = curr.getNext();
+			curr.setNext(node);
+			curr.getNext().setNext(prev);
+		}
 	}
 
 	/**
 	 * Append the nodes of the other list to the end of the curren list.
 	 */
 	public void appendList(LinkedList<T> ll) {
-
+		Node<T> secondhead = ll.head;
+		Node<T> curr = head;
+		while (null != curr.getNext()) {
+			curr = curr.getNext();
+		}
+		curr.setNext(secondhead);
 	}
 
 	/**
@@ -90,14 +137,36 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * elements, the extra element should be in the first list.
 	 */
 	public List<Node<T>> frontBackSplit() {
+		Node<T> curr = head;
+		Node<T> front = new Node<T>(curr.getData());
+		Node<T> temp = front;
+		int count = countNodes();
+		int split = (count % 2 == 0) ? count / 2 : count / 2 + 1;
 
+		while (split > 0) {
+			temp.setNext(new Node<T>(curr.getData()));
+			temp = temp.getNext();
+			curr = curr.getNext();
+			split--;
+		}
+
+		return Arrays.asList(front.getNext(), curr);
 	}
 
 	/**
 	 * Remove duplicates in a sorted list.
 	 */
 	public void removeDuplicates() {
-
+		Node<T> curr = head;
+		Node<T> next = null;
+		while (null != curr.getNext()) {
+			next = curr.getNext();
+			if (next.getData().compareTo(curr.getData()) == 0) {
+				curr.setNext(next.getNext());
+			} else {
+				curr = curr.getNext();
+			}
+		}
 	}
 
 	/**
@@ -105,7 +174,10 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * linked list as the destination's new head node.
 	 */
 	public void changeHead(LinkedList<T> destinationList) {
-
+		Node<T> curr = new Node<T>(popElement());
+		Node<T> dest = destinationList.head;
+		destinationList.head = curr;
+		destinationList.head.setNext(dest);
 	}
 
 	/**
@@ -113,7 +185,34 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * Assume the lists are sorted in ascending order.
 	 */
 	public LinkedList<T> sortedMergeList(LinkedList otherList) {
+		Node<T> curr = head;
+		Node<T> other = otherList.head;
+		Node<T> dummy = new Node<T>(curr.getData());
+		Node<T> sortedHead = dummy;
+		LinkedList<T> sortedList = new LinkedList<T>();
 
+		while (true) {
+			if (curr == null) {
+				sortedHead.setNext(other);
+				break;
+			}
+			if (other == null) {
+				sortedHead.setNext(curr);
+				break;
+			}
+
+			if (curr.getData().compareTo(other.getData()) <= 0) {
+				sortedHead.setNext(new Node<T>(curr.getData()));
+				curr = curr.getNext();
+			} else if (curr.getData().compareTo(other.getData()) > 0) {
+				sortedHead.setNext(new Node<T>(other.getData()));
+				other = other.getNext();
+			}
+
+			sortedHead = sortedHead.getNext();
+		}
+		sortedList.head = dummy.getNext();
+		return sortedList;
 	}
 
 	/**
@@ -121,7 +220,15 @@ public class LinkedList<T extends Comparable<T>> implements Cloneable {
 	 * first node.
 	 */
 	public void reverseList() {
-
+		Node<T> curr = head;
+		Node<T> prev = null;
+		while (null != curr) {
+			Node<T> next = curr.getNext();
+			curr.setNext(prev);
+			prev = curr;
+			curr = next;
+		}
+		head = prev;
 	}
 
 }
--- src/linkedlist/Main.java
+++ src/linkedlist/Main.java
@@ -4,87 +4,85 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        LinkedList<Integer> n = new LinkedList<Integer>();
-        n.addNode(3);
-        n.addNode(4);
-        n.addNode(5);
-        n.addNode(10);
+		LinkedList<Integer> n = new LinkedList<Integer>();
+		n.addNode(3);
+		n.addNode(4);
+		n.addNode(5);
+		n.addNode(10);
 
-        System.out.println("Printing all nodes through printnode function" );
-        n.printNodes(); //  to print all the nodes
+		System.out.println("Printing all nodes through printnode function");
+		n.printNodes(); // to print all the nodes
 
-        System.out.println("Using countNodes function, printing total number of nodes which is  " + n.countNodes());
+		System.out.println("\nUsing countNodes function, printing total number of nodes which is  " + n.countNodes());
 
-        System.out.println("Using popelemenet function,Popping the top node from the list whose value is " + n.popElement());
-        System.out.println("Printing all nodes through printnode function after pop function" );
-        n.printNodes();
+		System.out.println(
+				"Using pop elemenet function,Popping the top node from the list whose value is " + n.popElement());
+		System.out.println("Printing all nodes through printnode function after pop function");
+		n.printNodes();
 
+		n.insertNth(0, 3);
+		n.insertNth(4, 12);
+		n.insertNth(4, 11);
+		System.out.println("\nPrinting all nodes through printnode function after insert function");
+		n.printNodes();
 
-        n.insertNth(0, 3);
-        n.insertNth(4, 12);
-        n.insertNth(4, 11);
-        System.out.println("Printing all nodes through printnode function after insert function" );
-        n.printNodes();
+		n.insertSorted(3);
+		n.insertSorted(14);
+		n.insertSorted(14);
+		n.insertSorted(7);
+		System.out.println("\nPrinting all nodes through printnode function after insertsorted function");
+		n.printNodes();
 
-        n.insertSorted(3);
-        n.insertSorted(14);
-        n.insertSorted(14);
-        n.insertSorted(7);
-        System.out.println("Printing all nodes through printnode function after insertsorted function" );
-        n.printNodes();
+		// After removing duplicates
+		n.removeDuplicates();
+		System.out.println("\nPrinting all nodes through printnode function after removeDuplicate function");
+		n.printNodes();
 
-        // After removing duplicates
-        n.removeDuplicates();
-        System.out.println("Printing all nodes through printnode function after removeDuplicate function" );
-        n.printNodes();
+		LinkedList<Integer> n1 = new LinkedList<Integer>();
+		n1.addNode(500);
+		n1.addNode(600);
+		n1.addNode(700);
 
+		n.appendList(n1);
+		System.out.println("\nPrinting all nodes through printnode function after append_list function");
+		n.printNodes();
 
-        LinkedList<Integer> n1 = new LinkedList<Integer>();
-        n1.addNode(500);
-        n1.addNode(600);
-        n1.addNode(700);
+		n.changeHead(n1);
+		System.out.println("\nPrinting all nodes through printnode function after changeHead function");
+		n1.printNodes();
+		System.out.println("\n");
+		n.printNodes();
 
-        n.appendList(n1);
-        System.out.println("Printing all nodes through printnode function after append_list function" );
-        n.printNodes();
+		n.reverseList();
+		System.out.println("\nPrinting all nodes through printnode function after reverseLinked function");
+		n.printNodes();
+		n.reverseList();
+		LinkedList<Integer> mergesort = n.sortedMergeList(n1);
 
-        n.changeHead(n1);
-        System.out.println("Printing all nodes through printnode function after changeHead function" );
-        n1.printNodes();
-        n.printNodes();
+		System.out.println("\nPrinting all nodes through printnode function after mergesorted function");
+		mergesort.printNodes();
+ 
+		List<Node<Integer>> frontBackSplit = n.frontBackSplit();
 
-        n.reverseList();
-        System.out.println("Printing all nodes through printnode function after reverseLinked function" );
-        n.printNodes();
-        n.reverseList();
+		System.out
+				.println("\nPrinting all nodes through printnode function of Front Split after frontBackSplit function");
+		printNode(frontBackSplit.get(0));
 
-        LinkedList<Integer> mergesort = n.sortedMergeList(n1);
-        System.out.println("Printing all nodes through printnode function after mergesorted function" );
-        mergesort.printNodes();
+		System.out.println("\nPrinting all nodes through printnode function of Back Split after frontBackSplit function");
+		printNode(frontBackSplit.get(1));
+	}
 
-        List<Node<Integer>> frontBackSplit = n.frontBackSplit();
+	static void printNode(Node head) {
+		Node curr = null;
+		curr = head;
+		int i = 1;
+		while (curr != null) {
+			System.out.println("this is the Node number " + i + ", the value it stores is " + curr.toString());
+			curr = curr.getNext();
+			i++;
+		}
 
-        System.out.println("Printing all nodes through printnode function of Front Split after frontBackSplit function" );
-        printNode(frontBackSplit.get(0));
-
-        System.out.println("Printing all nodes through printnode function of Back Split after frontBackSplit function" );
-        printNode(frontBackSplit.get(1));
-    }
-
-
-    static void printNode(Node head)
-    {
-        Node curr = null;
-        curr = head;
-        int i = 1;
-        while (curr!= null)
-        {
-            System.out.println("this is the Node number " + i + ", the value it stores is "+ curr.toString() );
-            curr = curr.getNext();
-            i++;
-        }
-
-    }
+	}
 }
